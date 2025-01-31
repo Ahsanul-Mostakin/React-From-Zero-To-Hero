686faf73111f011915a0c899ba1494d492ce5e39
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-02-01 03:21:11 +0600

    attributes & styling

--- Getting Started/my-app/src/index.js
+++ Getting Started/my-app/src/index.js
@@ -1,18 +1,14 @@
+import './index.css'; 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const name = "Ahsanul Mostakin";
-const num = 10;
+//const name = "Ahsanul Mostakin";
+const img = "https://picsum.photos/200";
 root.render(
   <div>
-    <h1> {name} My Favorite Foods</h1>
-    <p>Your lucky number is { Math.floor (Math.random()* num + 1)}</p>
-    <ul>
-      <li>Bacon</li>
-      <li>Noodles</li>
-      <li>Jamon</li>
-    </ul>
+    <h1 className="heading"> My Favorite Foods are:</h1>
+    <img src = {img}/>
   </div>
 );
