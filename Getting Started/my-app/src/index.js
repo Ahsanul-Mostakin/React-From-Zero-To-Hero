a8004ebec58d25114d3d189bd7a9ea649cc9f563
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-02-01 02:11:39 +0600

    expressions

--- Getting Started/my-app/src/index.js
+++ Getting Started/my-app/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const name = "Ahsanul Mostakin";
+const num = 10;
 root.render(
   <div>
-    <h1>My Favorite Foods</h1>
+    <h1> {name} My Favorite Foods</h1>
+    <p>Your lucky number is { Math.floor (Math.random()* num + 1)}</p>
     <ul>
       <li>Bacon</li>
       <li>Noodles</li>
